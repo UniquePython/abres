95cfc045e2301c645bb74495949bb8e917496e12
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2026-03-15 19:17:24 +0530

    Implement result type, umbrella header, and cmake

---
+++ include/abres.h
@@ -0,0 +1,7 @@
+#ifndef ABRES_H
+#define ABRES_H
+
+#include "abres/error.h"
+#include "abres/result.h"
+
+#endif
---
+++ include/abres/result.h
@@ -0,0 +1,35 @@
+#ifndef ABRES_RESULT_H
+#define ABRES_RESULT_H
+
+#include "error.h"
+
+#include <stddef.h>
+
+#define DEF_RESULT(T)     \
+    typedef struct        \
+    {                     \
+        T value;          \
+        ABRES_Err *error; \
+    } Result_##T
+
+#define OK(T, val) \
+    (Result_##T) { .value = (val), .error = NULL }
+
+#define ERR(T, err) \
+    (Result_##T) { .value = {0}, .error = (err) }
+
+#define IS_OK(err) ((err).error == NULL)
+
+#define TRY(ReturnT, T, var, expr, wrap_code, ...)                                     \
+    T var;                                                                             \
+    do                                                                                 \
+    {                                                                                  \
+        Result_##T _r = (expr);                                                        \
+        if (!IS_OK(_r))                                                                \
+        {                                                                              \
+            return ERR(ReturnT, ABRES_NewErrFrom(wrap_code, _r.error, ##__VA_ARGS__)); \
+        }                                                                              \
+        var = _r.value;                                                                \
+    } while (0)
+
+#endif
